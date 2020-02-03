8d3dd822a0e0e52beda26cbef47ec9b323906b83
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-02-02 23:09:07 -0500

    added repl(), runFile(), and readFile()

--- lox/main.c
+++ lox/main.c
@@ -1,28 +1,85 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 #include "chunk.h"
 #include "common.h"
 #include "debug.h"
 #include "vm.h"
 
+#define REPL 1
+#define SOURCE 2
+#define LINE_MAX 1024
+#define READ "rb"
+#define EX_USAGE 64
+#define EX_DATAERR 65
+#define EX_SOFTWARE 70
+#define EX_IOERR 74
+
+static void repl();
+static void runFile(const char* path);
+static char* readFile(const char* path);
+
 int main(int argc, const char* argv[]) {
 	initVM();
-	Chunk chunk;
-	initChunk(&chunk);
-	int constant = addConstant(&chunk, 1.2);
-	writeChunk(&chunk, OP_CONSTANT, 123);
-	writeChunk(&chunk, constant, 123);
-	constant = addConstant(&chunk, 3.4);
-	writeChunk(&chunk, OP_CONSTANT, 123);
-	writeChunk(&chunk, constant, 123);
-	writeChunk(&chunk, OP_ADD, 123);
-	constant = addConstant(&chunk, 5.6);
-	writeChunk(&chunk, OP_CONSTANT, 123);
-	writeChunk(&chunk, constant, 123);
-	writeChunk(&chunk, OP_DIVIDE, 123);
-	writeChunk(&chunk, OP_NEGATE, 123);
-	writeChunk(&chunk, OP_RETURN, 123);
-	disassembleChunk(&chunk, "test chunk");
-	interpret(&chunk);
+	if (argc == REPL) {
+		repl();
+	}
+	else if (argc == SOURCE) {
+		runFile(argv[1]);
+	}
+	else {
+		fprintf(stderr, "Usage: lox [path]\n");
+		exit(EX_USAGE);
+	}
 	freeVM();
-	freeChunk(&chunk);
 	return 0;
+}
+
+void repl() {
+	char line[LINE_MAX];
+	while (true) {
+		printf("> ");
+		if (!fgets(line, sizeof(line), stdin)) {
+			printf("\n");
+			break;
+		}
+		interpret(line);
+	}
+}
+
+void runFile(const char* path) {
+	char* source = readFile(path);
+	InterpretResult result = interpret(source);
+	free(source);
+	if (result == INTERPRET_COMPILE_ERROR) {
+		exit(EX_DATAERR);
+	}
+	if (result == INTERPRET_RUNTIME_ERROR) {
+		exit(EX_SOFTWARE);
+	}
+}
+
+char* readFile(const char* path) {
+	FILE* file = fopen(path, READ);
+	if (!file) {
+		fprintf(stderr, "Could not open file \"%s\".\n", path);
+		exit(EX_IOERR);
+	}
+	fseek(file, 0L, SEEK_END);
+	size_t fileSize = ftell(file);
+	rewind(file);
+	char* buffer = (char*)malloc(fileSize + 1);
+	if (!buffer) {
+		fprintf(stderr, "Not enough memory to read \"%s\".\n", path);
+		exit(EX_IOERR);
+	}
+	size_t bytesRead = fread(buffer, sizeof(char), fileSize, file);
+	if (bytesRead < fileSize) {
+		fprintf(stderr, "Could not read file \"%s\".\n", path);
+		exit(EX_IOERR);
+	}
+	buffer[bytesRead] = '\0';
+	fclose(file);
+	return buffer;
 }
