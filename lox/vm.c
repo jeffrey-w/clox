2134a4f3829f055c1be567396a056376f0020aab
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-02-04 09:35:10 -0500

    added support for logical negation and comparasion operators

--- lox/vm.c
+++ lox/vm.c
@@ -1,3 +1,4 @@
+#include <stdarg.h>
 #include <stdio.h>
 
 #include "chunk.h"
@@ -9,6 +10,9 @@
 
 static InterpretResult run();
 static void resetStack();
+static Value peek(int distance);
+static bool isFalsey(Value value);
+static void runtimeError(const char* format, ...);
 
 void initVM() {
 	resetStack();
@@ -39,12 +43,16 @@ InterpretResult interpret(const char* source) {
 InterpretResult run() {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
-#define BINARY_OP(op) \
-	do { \
-		double b = pop(); \
-		double a = pop(); \
-		push(a op b); \
-	} while (false)
+#define BINARY_OP(valueType, op) \
+    do { \
+      if (!IS_NUMBER(peek(0)) || !IS_NUMBER(peek(1))) { \
+        runtimeError("Operands must be numbers."); \
+        return INTERPRET_RUNTIME_ERROR; \
+      } \
+      double b = AS_NUMBER(pop()); \
+      double a = AS_NUMBER(pop()); \
+      push(valueType(a op b)); \
+    } while (false)
 	while (true) {
 #ifdef DEBUG_TRACE_EXECUTION
 		printf("          ");
@@ -63,20 +71,48 @@ InterpretResult run() {
 			push(constant);
 			break;
 		}
+		case OP_NIL:
+			push(NIL_VAL);
+			break;
+		case OP_TRUE:
+			push(BOOL_VAL(true));
+			break;
+		case OP_FALSE:
+			push(BOOL_VAL(false));
+			break;
+		case OP_EQUAL: {
+			Value b = pop();
+			Value a = pop();
+			push(BOOL_VAL(valuesEqual(a, b)));
+			break;
+		}
+		case OP_GREATER:
+			BINARY_OP(BOOL_VAL, > );
+			break;
+		case OP_LESS:
+			BINARY_OP(BOOL_VAL, < );
+			break;
 		case OP_ADD:
-			BINARY_OP(+);
+			BINARY_OP(NUMBER_VAL, +);
 			break;
 		case OP_SUBTRACT:
-			BINARY_OP(-);
+			BINARY_OP(NUMBER_VAL, -);
 			break;
 		case OP_MULTIPLY:
-			BINARY_OP(*);
+			BINARY_OP(NUMBER_VAL, *);
 			break;
 		case OP_DIVIDE:
-			BINARY_OP(/);
+			BINARY_OP(NUMBER_VAL, /);
+			break;
+		case OP_NOT:
+			push(BOOL_VAL(isFalsey(pop())));
 			break;
 		case OP_NEGATE:
-			push(-pop());
+			if (!IS_NUMBER(peek(0))) {
+				runtimeError("Operand must be a number.");
+				return INTERPRET_RUNTIME_ERROR;
+			}
+			push(NUMBER_VAL(-AS_NUMBER(pop())));
 			break;
 		case OP_RETURN:
 			printValue(pop());
@@ -95,4 +131,24 @@ void push(Value value) {
 
 Value pop() {
 	return *(--vm.stackTop);
+}
+
+Value peek(int distance) {
+	return vm.stackTop[-1 - distance];
+}
+
+bool isFalsey(Value value) {
+	return IS_NIL(value) || (IS_BOOL(value) && !AS_BOOL(value));
+}
+
+void runtimeError(const char* format, ...) {
+	va_list args;
+	va_start(args, format);
+	vfprintf(stderr, format, args);
+	va_end(args);
+	fputs("\n", stderr);
+	size_t instruction = vm.ip - vm.chunk->code;
+	int line = vm.chunk->lines[instruction];
+	fprintf(stderr, "[line %d] in script\n", line);
+	resetStack();
 }
