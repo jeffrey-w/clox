5715d60465a92ed4bd83a4a34ae0491ee96b0659
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-02-04 09:30:51 -0500

    added literal() and binary(), modified unary()

--- lox/compiler.c
+++ lox/compiler.c
@@ -11,6 +11,7 @@ static void consume(TokenType type, const char* message);
 static void expression();
 static void parsePrecedence(Precedence precedence);
 static ParseRule* getRule(TokenType type);
+static void literal();
 static void number();
 static void unary();
 static void binary();
@@ -38,31 +39,31 @@ ParseRule rules[] = {
   { NULL,     NULL,    PREC_NONE },       // TOKEN_SEMICOLON       
   { NULL,     binary,  PREC_FACTOR },     // TOKEN_SLASH           
   { NULL,     binary,  PREC_FACTOR },     // TOKEN_STAR            
-  { NULL,     NULL,    PREC_NONE },       // TOKEN_BANG            
-  { NULL,     NULL,    PREC_NONE },       // TOKEN_BANG_EQUAL      
+  { unary,    NULL,    PREC_NONE },       // TOKEN_BANG            
+  { NULL,     binary,  PREC_COMPARISON }, // TOKEN_BANG_EQUAL      
   { NULL,     NULL,    PREC_NONE },       // TOKEN_EQUAL           
-  { NULL,     NULL,    PREC_NONE },       // TOKEN_EQUAL_EQUAL     
-  { NULL,     NULL,    PREC_NONE },       // TOKEN_GREATER         
-  { NULL,     NULL,    PREC_NONE },       // TOKEN_GREATER_EQUAL   
-  { NULL,     NULL,    PREC_NONE },       // TOKEN_LESS            
-  { NULL,     NULL,    PREC_NONE },       // TOKEN_LESS_EQUAL      
+  { NULL,     binary,  PREC_COMPARISON }, // TOKEN_EQUAL_EQUAL     
+  { NULL,     binary,  PREC_COMPARISON }, // TOKEN_GREATER         
+  { NULL,     binary,  PREC_COMPARISON }, // TOKEN_GREATER_EQUAL   
+  { NULL,     binary,  PREC_COMPARISON }, // TOKEN_LESS            
+  { NULL,     binary,  PREC_COMPARISON }, // TOKEN_LESS_EQUAL      
   { NULL,     NULL,    PREC_NONE },       // TOKEN_IDENTIFIER      
   { NULL,     NULL,    PREC_NONE },       // TOKEN_STRING          
   { number,   NULL,    PREC_NONE },       // TOKEN_NUMBER          
   { NULL,     NULL,    PREC_NONE },       // TOKEN_AND             
   { NULL,     NULL,    PREC_NONE },       // TOKEN_CLASS           
   { NULL,     NULL,    PREC_NONE },       // TOKEN_ELSE            
-  { NULL,     NULL,    PREC_NONE },       // TOKEN_FALSE           
+  { literal,  NULL,    PREC_NONE },       // TOKEN_FALSE           
   { NULL,     NULL,    PREC_NONE },       // TOKEN_FOR             
   { NULL,     NULL,    PREC_NONE },       // TOKEN_FUN             
   { NULL,     NULL,    PREC_NONE },       // TOKEN_IF              
-  { NULL,     NULL,    PREC_NONE },       // TOKEN_NIL             
+  { literal,  NULL,    PREC_NONE },       // TOKEN_NIL             
   { NULL,     NULL,    PREC_NONE },       // TOKEN_OR              
   { NULL,     NULL,    PREC_NONE },       // TOKEN_PRINT           
   { NULL,     NULL,    PREC_NONE },       // TOKEN_RETURN          
   { NULL,     NULL,    PREC_NONE },       // TOKEN_SUPER           
   { NULL,     NULL,    PREC_NONE },       // TOKEN_THIS            
-  { NULL,     NULL,    PREC_NONE },       // TOKEN_TRUE            
+  { literal,  NULL,    PREC_NONE },       // TOKEN_TRUE            
   { NULL,     NULL,    PREC_NONE },       // TOKEN_VAR             
   { NULL,     NULL,    PREC_NONE },       // TOKEN_WHILE           
   { NULL,     NULL,    PREC_NONE },       // TOKEN_ERROR           
@@ -123,15 +124,34 @@ ParseRule* getRule(TokenType type) {
 	return &rules[type];
 }
 
+void literal() {
+	switch (parser.previous.type) {
+	case TOKEN_FALSE:
+		emitByte(OP_FALSE);
+		break;
+	case TOKEN_NIL:
+		emitByte(OP_NIL);
+		break;
+	case TOKEN_TRUE:
+		emitByte(OP_TRUE);
+		break;
+	default:
+		return; // TODO need internal error logic
+	}
+}
+
 void number() {
 	double value = strtod(parser.previous.start, NULL);
-	emitConstant(value);
+	emitConstant(NUMBER_VAL(value));
 }
 
 void unary() {
 	TokenType operatorType = parser.previous.type;
 	parsePrecedence(PREC_UNARY);
 	switch (operatorType) {
+	case TOKEN_BANG:
+		emitByte(OP_NOT);
+		break;
 	case TOKEN_MINUS:
 		emitByte(OP_NEGATE);
 		break;
@@ -145,6 +165,24 @@ void binary() {
 	ParseRule* rule = getRule(operatorType);
 	parsePrecedence((Precedence)(rule->precedence + 1));
 	switch (operatorType) {
+	case TOKEN_BANG_EQUAL:
+		emitBytes(OP_EQUAL, OP_NOT);
+		break;
+	case TOKEN_EQUAL_EQUAL:
+		emitByte(OP_EQUAL);
+		break;
+	case TOKEN_GREATER:
+		emitByte(OP_GREATER);
+		break;
+	case TOKEN_GREATER_EQUAL:
+		emitBytes(OP_LESS, OP_NOT);
+		break;
+	case TOKEN_LESS:
+		emitByte(OP_LESS);
+		break;
+	case TOKEN_LESS_EQUAL:
+		emitBytes(OP_GREATER, OP_NOT);
+		break;
 	case TOKEN_PLUS:
 		emitByte(OP_ADD);
 		break;
