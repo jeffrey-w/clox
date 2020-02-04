eaef9d755963fc69cfac8338b4fa87a1bd6d59c8
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-02-04 09:33:35 -0500

    added valuesEqual()

--- lox/value.c
+++ lox/value.c
@@ -23,6 +23,34 @@ void writeValueArray(ValueArray* array, Value value) {
 	array->values[array->count++] = value;
 }
 
+bool valuesEqual(Value a, Value b) {
+	if (a.type != b.type) {
+		return false;
+	}
+	switch (a.type) {
+	case VAL_BOOL:
+		return AS_BOOL(a) == AS_BOOL(b);
+	case VAL_NIL:
+		return true;
+	case VAL_NUMBER:
+		return AS_NUMBER(a) == AS_NUMBER(b);
+	default:
+		break;
+	}
+}
+
 void printValue(Value value) {
-	printf("%g", value);
+	switch (value.type) {
+	case VAL_BOOL:
+		printf(AS_BOOL(value) ? "true" : "false");
+		break;
+	case VAL_NIL:
+		printf("nil");
+		break;
+	case VAL_NUMBER:
+		printf("%g", AS_NUMBER(value));
+		break;
+	default:
+		return; // TODO need internal error logic
+	}
 }
