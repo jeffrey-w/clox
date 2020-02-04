784547f8cbc16e7516a7710db15c89d942f75eda
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-02-04 09:34:37 -0500

    added ValueType, expanded Value to support multiple types

--- lox/value.h
+++ lox/value.h
@@ -2,7 +2,28 @@
 
 #include "common.h"
 
-typedef double Value;
+#define IS_BOOL(value)    ((value).type == VAL_BOOL)  
+#define IS_NIL(value)     ((value).type == VAL_NIL)   
+#define IS_NUMBER(value)  ((value).type == VAL_NUMBER)
+#define AS_BOOL(value)    ((value).as.boolean)                       
+#define AS_NUMBER(value)  ((value).as.number)   
+#define BOOL_VAL(value)   ((Value){ VAL_BOOL, { .boolean = value } }) 
+#define NIL_VAL           ((Value){ VAL_NIL, { .number = 0 } })       
+#define NUMBER_VAL(value) ((Value){ VAL_NUMBER, { .number = value } })
+
+typedef enum {
+	VAL_BOOL,
+	VAL_NIL,
+	VAL_NUMBER
+} ValueType;
+
+typedef struct {
+	ValueType type;
+	union {
+		bool boolean;
+		double number;
+	} as;
+} Value;
 
 typedef struct {
 	int capacity;
@@ -13,4 +34,5 @@ typedef struct {
 void initValueArray(ValueArray* array);
 void freeValueArray(ValueArray* array);
 void writeValueArray(ValueArray* array, Value value);
+bool valuesEqual(Value a, Value b);
 void printValue(Value value);
