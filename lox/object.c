b5c5ebdfe4b9957a9dcb12e2800e7d71ac2846fa
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-02-04 22:08:25 -0500

    added function printObject()

--- lox/object.c
+++ lox/object.c
@@ -1,11 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
 #include "memory.h"
 #include "object.h"
+#include "value.h"
+#include "vm.h"
 
 #define ALLOCATE_OBJ(type, objectType) \
 	(type*)allocateObject(sizeof(type), objectType);
 
 static Obj* allocateObject(size_t size, ObjType type);
 static ObjString* allocateString(char* data, int length);
+
+void printObject(Value value) {
+	switch (value.type) {
+	case OBJ_STRING:
+		printf("%s", AS_CSTRING(value));
+	default:
+		break; // TODO need internal error logic
+	}
+}
 
 ObjString* copyString(const char* string, int length) {
 	char* data = ALLOCATE(char, length + 1);
