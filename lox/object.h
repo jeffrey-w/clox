63c406c31108818b9e82656296e976f7f9ebe822
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-02-04 21:16:53 -0500

    added macros IS_STRING, AS_STRING, and AS_CSTRING; and function isObjectType()

--- lox/object.h
+++ lox/object.h
@@ -5,6 +5,8 @@
 
 #define OBJ_TYPE(value)         (AS_OBJ(value)->type)
 #define IS_STRING(value)        isObjType(value, OBJ_STRING)
+#define AS_STRING(value)        ((ObjString*)AS_OBJ(value))         
+#define AS_CSTRING(value)       (AS_STRING(value)->chars)
 
 typedef enum {
 	OBJ_STRING
