2c73b3c0ead8fd2d4436b1165c3e2daef0b06e0a
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-02-06 13:20:18 -0500

    added functions tableSet(), adjustCapacity(), and findEntry()

--- lox/table.c
+++ lox/table.c
@@ -6,6 +6,11 @@
 #include "table.h"
 #include "value.h"
 
+#define TABLE_MAX_LOAD 0.75
+
+static void adjustCapacity(Table* table, int capacity);
+static Entry* findEntry(Entry* entries, int capacity, ObjString* key);
+
 void initTable(Table* table) {
 	table->count = 0;
 	table->capacity = 0;
@@ -15,4 +20,40 @@ void initTable(Table* table) {
 void freeTable(Table* table) {
 	FREE_ARRAY(Entry, table->entries, table->capacity);
 	initTable(table);
+}
+
+bool tableSet(Table* table, ObjString* key, Value value) {
+	if (table->capacity * TABLE_MAX_LOAD < table->count + 1) {
+		int capcity = GROW_CAPACITY(table->capacity);
+		adjustCapacity(table, capcity);
+	}
+	Entry* entry = findEntry(table->entries, table->capacity, key);
+	bool isNewKey = entry->key == NULL;
+	if (isNewKey) {
+		table->count++;
+	}
+	entry->key = key;
+	entry->value = value;
+	return isNewKey;
+}
+
+void adjustCapacity(Table* table, int capacity) {
+	Entry* entries = ALLOCATE(Entry, capacity);
+	for (int i = 0; i < capacity; i++) {
+		entries[i].key = NULL;
+		entries[i].value = NIL_VAL;
+	}
+	table->entries = entries;
+	table->capacity = capacity;
+}
+
+Entry* findEntry(Entry* entries, int capacity, ObjString* key) {
+	uint32_t index = key->hash & (capacity - 1);
+	while (true) {
+		Entry* entry = &entries[index];
+		if (entry->key == key || entry->key == NULL) {
+			return entry;
+		}
+		index = (index + 1) & (capacity - 1);
+	}
 }
