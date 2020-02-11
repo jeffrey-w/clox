9b976f5b3f72abdc8866f1c86e3d8dd19e129e7f
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-02-11 00:09:32 -0500

    add forStatement()

--- lox/compiler.c
+++ lox/compiler.c
@@ -10,6 +10,8 @@
 
 #define UNINITIALIZED -1
 
+// TODO add support for switch statements and the ternary operator
+
 static void initComplier(Compiler*);
 static void advance();
 static void consume(TokenType, const char*);
@@ -27,6 +29,7 @@ static void printStatement();
 static void ifStatement();
 static void patchJump(int offset);
 static void whileStatement();
+static void forStatement();
 static void block();
 static void beginScope();
 static void endScope();
@@ -237,6 +240,9 @@ void statement() {
 	else if (match(TOKEN_WHILE)) {
 		whileStatement();
 	}
+	else if (match(TOKEN_FOR)) {
+		forStatement();
+	}
 	else if (match(TOKEN_LEFT_BRACE)) {
 		beginScope();
 		block();
@@ -290,6 +296,44 @@ void whileStatement() {
 	emitLoop(loopStart);
 	patchJump(exitJump);
 	emitByte(OP_POP);
+}
+
+void forStatement() {
+	int loopStart, exitJump;
+	beginScope();
+	consume(TOKEN_LEFT_PAREN, "Expect '(' after 'for'.");
+	if (match(TOKEN_SEMICOLON)); // No initializer.
+	else if (match(TOKEN_VAR)) {
+		varDeclaration();
+	}
+	else {
+		expressionStatement();
+	}
+	loopStart = currentChunk()->count;
+	exitJump = UNINITIALIZED;
+	if (!match(TOKEN_SEMICOLON)) {
+		expression();
+		consume(TOKEN_SEMICOLON, "Expect ';' after loop condition.");
+		exitJump = emitJump(OP_JUMP_IF_FALSE);
+		emitByte(OP_POP);
+	}
+	if (!match(TOKEN_RIGHT_PAREN)) {
+		int bodyJump = emitJump(OP_JUMP);
+		int incrementStart = currentChunk()->count;
+		expression();
+		emitByte(OP_POP);
+		consume(TOKEN_RIGHT_PAREN, "Expect ')' after for clauses.");
+		emitLoop(loopStart);
+		loopStart = incrementStart;
+		patchJump(bodyJump);
+	}
+	statement();
+	emitLoop(loopStart);
+	if (exitJump != UNINITIALIZED) {
+		patchJump(exitJump);
+		emitByte(OP_POP);
+	}
+	endScope();
 }
 
 void block() {
