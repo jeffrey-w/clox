4e1b907a11254f37ad499ceaac44f6fbe7ef7e71
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-02-10 22:30:40 -0500

    added support for OP_JUMP and OP_JUMP_IF_FALSE

--- lox/debug.c
+++ lox/debug.c
@@ -6,6 +6,7 @@
 static int simpleInstruction(const char*, int);
 static int constantInstruction(const char*, Chunk*, int);
 static int byteInstruction(const char*, Chunk*, int);
+static int jumpInstruction(const char*, int, Chunk*, int);
 
 void disassembleChunk(Chunk* chunk, const char* name) {
 	printf("== %s ==\n", name);
@@ -65,6 +66,10 @@ int disassembleInstruction(Chunk* chunk, int offset) {
 		return simpleInstruction("OP_NEGATE", offset);
 	case OP_PRINT:
 		return simpleInstruction("OP_PRINT", offset);
+	case OP_JUMP:
+		return jumpInstruction("OP_JUMP", 1, chunk, offset);
+	case OP_JUMP_IF_FALSE:
+		return jumpInstruction("OP_JUMP_IF_FALSE", 1, chunk, offset);
 	case OP_RETURN:
 		return simpleInstruction("OP_RETURN", offset);
 	default:
@@ -90,4 +95,11 @@ int byteInstruction(const char* name, Chunk* chunk, int offset) {
 	uint8_t slot = chunk->code[offset + 1];
 	printf("%-16s %4d\n", name, slot);
 	return offset + 2;
+}
+
+int jumpInstruction(const char* name, int sign, Chunk* chunk, int offset) {
+	uint16_t jump = (uint16_t)(chunk->code[offset + 1] << 8);
+	jump |= chunk->code[offset + 2];
+	printf("%-16s %4d -> %d\n", name, offset, offset + 3 + sign * jump);
+	return offset + 3;
 }
