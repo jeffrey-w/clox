4cf1d20d2c7d63089c741a77ab67d33802074dbd
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-02-12 16:43:43 -0500

    added struct CallFrame

--- lox/vm.h
+++ lox/vm.h
@@ -1,10 +1,12 @@
 #pragma once
 
 #include "chunk.h"
+#include "object.h"
 #include "table.h"
 #include "value.h"
 
-#define STACK_MAX 256
+#define FRAMES_MAX 64
+#define STACK_MAX (FRAMES_MAX * UINT8_COUNT)
 
 typedef enum {
 	INTERPRET_OK,
@@ -13,8 +15,14 @@ typedef enum {
 } InterpretResult;
 
 typedef struct {
-	Chunk* chunk;
+	ObjFunction* function;
 	uint8_t* ip;
+	Value* slots;
+} CallFrame;
+
+typedef struct {
+	CallFrame frames[FRAMES_MAX];
+	int frameCount;
 	Value stack[STACK_MAX]; // TODO need to handle stack overflow
 	Value* stackTop;
 	Table globals;
