a50e049bf386ebb20e21e964d29e85e75459766b
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-02-14 10:38:29 -0500

    added case for OBJ_NATIVE to freeObject()

--- lox/memory.c
+++ lox/memory.c
@@ -32,6 +32,9 @@ void freeObject(Obj* object) {
 		FREE(ObjString, object);
 		break;
 	}
+	case OBJ_NATIVE:
+		FREE(ObjNative, object);
+		break;
 	case OBJ_FUNCTION: {
 		ObjFunction* function = (ObjFunction*)object;
 		freeChunk(&function->chunk);
