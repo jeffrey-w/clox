546652265f8a13dbf74476f3214f301fb035edf2
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-02-16 12:04:00 -0500

    added scanNative()

--- lox/natives.c
+++ lox/natives.c
@@ -1,7 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 
 #include "natives.h"
 
 Value clockNative(int argCount, Value* args) {
 	return NUMBER_VAL((double)clock() / CLOCKS_PER_SEC);
+}
+
+Value scanNative(int argCount, Value* args) { // TODO this is not robust (e.g. fails if input is typed too fast)
+	size_t size = 0, len = 0;
+	char* buffer = NULL;
+	do {
+		if (size < len + 1) {
+			int old = size;
+			int size = GROW_CAPACITY(old);
+			buffer = GROW_ARRAY(buffer, char, old, size);
+		}
+		buffer[len++] = fgetc(stdin);
+	} while(buffer[len - 1] != '\n');
+	buffer[len - 1] = '\0';
+	return OBJ_VAL(takeString(buffer, len - 1));
 }
--- lox/natives.h
+++ lox/natives.h
@@ -1,6 +1,10 @@
 #pragma once
 
+#include <string.h>
+
+#include "memory.h"
 #include "value.h"
 
 Value clockNative(int, Value*);
+Value scanNative(int, Value*);
 // TODO scan and sin
