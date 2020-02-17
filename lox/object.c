f6573d5b027355685dda84ce546083015e9ccfa9
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-02-17 00:10:12 -0500

    modified newFunction()

--- lox/object.c
+++ lox/object.c
@@ -101,6 +101,7 @@ ObjNative* newNative(NativeFn function) {
 ObjFunction* newFunction() {
 	ObjFunction* function = ALLOCATE_OBJ(ObjFunction, OBJ_FUNCTION);
 	function->arity = 0;
+	function->upvalueCount = 0;
 	function->name = NULL;
 	initChunk(&function->chunk);
 	return function;
