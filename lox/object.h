48ba535d452e808c5c1cdb1bd17b89149d0ae5b2
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-02-17 16:50:49 -0500

    added memeber next to ObjUpvalue

--- lox/object.h
+++ lox/object.h
@@ -38,6 +38,7 @@ struct sObjString { // TODO take 'const' strings from source
 typedef struct sObjUpvalue {
 	Obj obj;
 	Value* location;
+	struct sObjUpvalue* next;
 } ObjUpvalue;
 
 typedef Value (*NativeFn)(int, Value*);
