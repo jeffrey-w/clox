880960d5f96438a33f63453c93c4f556cdcae5e7
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-03-10 17:41:15 -0400

    add DEBUG_LOG_GC

--- lox/common.h
+++ lox/common.h
@@ -7,6 +7,7 @@
 #define DEBUG_TRACE_EXECUTION
 #define DEBUG_PRINT_CODE
 #define DEBUG_STRESS_GC
+#define DEBUG_LOG_GC
 #define UINT8_COUNT (UINT8_MAX + 1)
 
 // TODO use consistent NULL/0 check in control expressions
