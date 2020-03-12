8316c4547c0859c977f2bbc873b03aac73be117a
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-03-12 11:52:43 -0400

    switch off DEBUG_TRACE_EXECUTION, DEBUG_PRINT_CODE, and DEBUG_LOG_GC

--- lox/common.h
+++ lox/common.h
@@ -4,10 +4,10 @@
 #include <stddef.h>
 #include <stdint.h>
 
-#define DEBUG_TRACE_EXECUTION
-#define DEBUG_PRINT_CODE
+//#define DEBUG_TRACE_EXECUTION
+//#define DEBUG_PRINT_CODE
 //#define DEBUG_STRESS_GC
-#define DEBUG_LOG_GC
+//#define DEBUG_LOG_GC
 #define DEBUG_DIAG_TOOLS
 #define UINT8_COUNT (UINT8_MAX + 1)
 
