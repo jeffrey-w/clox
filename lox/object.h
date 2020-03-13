294256fb43ebfcd4ef52958a2e5157b39ab5c744
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-03-12 21:34:21 -0400

    add ObjBoundMethod and newBoundMethod()

--- lox/object.h
+++ lox/object.h
@@ -11,13 +11,15 @@
 #define IS_FUNCTION(value)      isObjType(value, OBJ_FUNCTION)
 #define IS_CLOSURE(value)       isObjType(value, OBJ_CLOSURE)
 #define IS_CLASS(value)         isObjType(value, OBJ_CLASS)
+#define IS_BOUND_METHOD(value)  isObjType(value, OBJ_BOUND_METHOD)
 #define IS_INSTANCE(value)      isObjType(value, OBJ_INSTANCE)
 #define AS_STRING(value)        ((ObjString*)AS_OBJ(value))        
 #define AS_CSTRING(value)       (AS_STRING(value)->data)
 #define AS_NATIVE(value)        (((ObjNative*)AS_OBJ(value))->function)
 #define AS_FUNCTION(value)      ((ObjFunction*)AS_OBJ(value))
 #define AS_CLOSURE(value)       ((ObjClosure*)AS_OBJ(value)) 
-#define AS_CLASS(value)         ((ObjClass*)AS_OBJ(value)) 
+#define AS_CLASS(value)         ((ObjClass*)AS_OBJ(value))
+#define AS_BOUND_METHOD(value)  ((ObjBoundMethod*)AS_OBJ(value))
 #define AS_INSTANCE(value)      ((ObjInstance*)AS_OBJ(value))   
 
 typedef enum {
@@ -27,6 +29,7 @@ typedef enum {
 	OBJ_FUNCTION,
 	OBJ_CLOSURE,
 	OBJ_CLASS,
+	OBJ_BOUND_METHOD,
 	OBJ_INSTANCE
 } ObjType;
 
@@ -80,6 +83,12 @@ typedef struct sObjClass {
 
 typedef struct {
 	Obj obj;
+	Value receiver;
+	ObjClosure* method;
+} ObjBoundMethod;
+
+typedef struct {
+	Obj obj;
 	ObjClass* cls;
 	Table fields;
 } ObjInstance;
@@ -92,6 +101,7 @@ ObjNative* newNative(NativeFn);
 ObjFunction* newFunction();
 ObjClosure* newClosure(ObjFunction*);
 ObjClass* newClass(ObjString*);
+ObjBoundMethod* newBoundMethod(Value, ObjClosure*);
 ObjInstance* newInstance(ObjClass*);
 
 static inline bool isObjType(Value value, ObjType type) {
