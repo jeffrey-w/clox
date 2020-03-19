4f37c202bbf309a2b2a74022726188ef085c4304
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-03-19 00:26:27 -0400

    disable DEBUG_DIAG_TOOLS

--- lox/common.h
+++ lox/common.h
@@ -8,7 +8,7 @@
 //#define DEBUG_PRINT_CODE
 //#define DEBUG_STRESS_GC
 //#define DEBUG_LOG_GC
-#define DEBUG_DIAG_TOOLS
+//#define DEBUG_DIAG_TOOLS
 #define UINT8_COUNT (UINT8_MAX + 1)
 
 // TODO use consistent NULL/0 check in control expressions
