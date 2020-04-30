3ffd4d21a9098458a11da9eecf8d2480df8be66a
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-04-29 22:33:46 -0400

    add STRCMP

--- lox/common.h
+++ lox/common.h
@@ -11,5 +11,8 @@
 //#define DEBUG_DIAG_TOOLS
 #define UINT8_COUNT (UINT8_MAX + 1)
 
+#define STRCMP(a, b, c, d) \
+	(a) == (b) && !memcmp((c), (d), (a))
+
 // TODO use consistent NULL/0 check in control expressions
 // TODO hoist "private" globals to implementation files
