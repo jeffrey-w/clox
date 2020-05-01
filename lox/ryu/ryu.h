53e9bad130b936122f048302d7204fe6df00b1a9
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-05-01 12:18:57 -0400

    remove unused functions

--- lox/ryu/ryu.h
+++ lox/ryu/ryu.h
@@ -23,17 +23,9 @@ extern "C" {
 
 #include <inttypes.h>
 
-int d2s_buffered_n(double f, char* result);
-void d2s_buffered(double f, char* result);
-char* d2s(double f);
-
 int d2fixed_buffered_n(double d, uint32_t precision, char* result);
 void d2fixed_buffered(double d, uint32_t precision, char* result);
 char* d2fixed(double d, uint32_t precision);
-
-int d2exp_buffered_n(double d, uint32_t precision, char* result);
-void d2exp_buffered(double d, uint32_t precision, char* result);
-char* d2exp(double d, uint32_t precision);
 
 #ifdef __cplusplus
 }
